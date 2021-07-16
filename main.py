cc627fd94023d352630ffb99f0ceda297c97bbaf
Desktop Casa Arg <hbermudez.info@gmail.com>
2021-07-16 03:00:02 -0300

    Carriles centrados para la nave

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     vertical_lines = []
-    V_NB_LINES = 7
+    V_NB_LINES = 4
     V_LINES_SPACING = .1 # porcentaje del ancho de la pantalla
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -44,7 +44,7 @@ class MainWidget(Widget):
         central_line_x = int(self.width/2)
         spacing = self.V_LINES_SPACING * self.width
         # self.line.points = [center_x, 0, center_x, 100]
-        offset = -int(self.V_NB_LINES/2)
+        offset = -int(self.V_NB_LINES/2) + 0.5
         for i in range(0, self.V_NB_LINES):
             line_x = int(central_line_x + offset*spacing)
             x1, y1 = self.transform(line_x, 0)
@@ -53,8 +53,8 @@ class MainWidget(Widget):
             offset += 1
 
     def transform(self, x, y):
-        #  return self.transform_2D(x, y)
-        return self.transform_perspective(x, y)
+        return self.transform_2D(x, y)
+        # return self.transform_perspective(x, y)
 
     def transform_2D(self, x, y):
         return int(x), int(y)
