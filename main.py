8e04cb370a04a834ed685b040b611aa2a24311f0
Desktop Casa Arg <hbermudez.info@gmail.com>
2021-07-16 02:54:05 -0300

    x, y con dinamica con la transformacion

--- main.py
+++ main.py
@@ -57,13 +57,19 @@ class MainWidget(Widget):
         return self.transform_perspective(x, y)
 
     def transform_2D(self, x, y):
-        return x, y
+        return int(x), int(y)
 
     def transform_perspective(self, x, y):
         tr_y = y * self.perspective_point_y / self.height
         if tr_y > self.perspective_point_y:
             tr_y = self.perspective_point_y
-        return x, tr_y
+
+        diff_x = x - self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y / self.perspective_point_y
+
+        tr_x = self.perspective_point_x + diff_x * proportion_y
+        return int(tr_x), int(tr_y)
 
 
 class GalaxyApp(App):
