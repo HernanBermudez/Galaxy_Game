486898c0052d0e629eaef75b5158dbfa3ff9d9a9
HernanMac <hbermudez.info@gmail.com>
2021-07-18 20:20:27 -0300

    Nave y camino designado

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from kivy.config import Config
 
 Config.set('graphics', 'width', '900')
@@ -6,7 +8,7 @@ Config.set('graphics', 'height', '400')
 from kivy import platform
 from kivy.core.window import Window
 from kivy.app import App
-from kivy.graphics import Color, Line, Quad
+from kivy.graphics import Color, Line, Quad, Triangle
 from kivy.properties import NumericProperty, Clock
 from kivy.uix.widget import Widget
 
@@ -17,29 +19,36 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
     vertical_lines = []
-    V_NB_LINES = 4
-    V_LINES_SPACING = .1 # porcentaje del ancho de la pantalla
+    V_NB_LINES = 8
+    V_LINES_SPACING = .2 # porcentaje del ancho de la pantalla
 
     horizontal_lines = []
     H_NB_LINES = 8
     H_LINES_SPACING = .1  # porcentaje del ancho de la pantalla
 
     current_offset_y = 0
-    speed = .5
+    speed = 1
     speed_x = 12
     current_speed_x = 0
     current_offset_x = 0
     current_y_loop = 0
 
-    NB_TILES = 8
+    NB_TILES = 16
     tiles = []
     tiles_coordinates = []
+
+    SHIP_WIDTH = .1
+    SHIP_HEIGHT = 0.035
+    SHIP_BASE_Y = 0.04
+    ship = None
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
+        self.init_ship()
+        self.pre_fill_tiles_coordinates()
         self.generate_tiles_coordinates()
         # print("INIT W:" +str(self.width) + "H:" + str(self.height))
         if self.is_desktop():
@@ -54,15 +63,77 @@ class MainWidget(Widget):
             return True
         return False
 
+    def init_ship(self):
+        with self.canvas:
+            Color(0, 0, 0)
+            self.ship = Triangle()
+
+    def update_ship(self):
+
+        center_x = self.width / 2
+        base_y = self.SHIP_BASE_Y * self.height
+        ship_half_width = self.SHIP_WIDTH * self.width / 2
+        ship_height = self.SHIP_HEIGHT * self.height
+
+        #    2
+        # 1     3
+        # self.transform
+        x1, y1 = self.transform(center_x - ship_half_width, base_y )
+        x2, y2 = self.transform(center_x, base_y + ship_height)
+        x3, y3 = self.transform(center_x + ship_half_width, base_y)
+        self.ship.points = [x1, y1, x2, y2, x3, y3]
+
     def init_tiles(self):
         with self.canvas:
             Color(1, 1, 1)
             for i in range (0, self.NB_TILES):
                 self.tiles.append(Quad())
 
-    def generate_tiles_coordinates(self):
-        for i in range (0, self.NB_TILES):
+    def pre_fill_tiles_coordinates(self):
+        for i in range (0,10):
             self.tiles_coordinates.append((0, i))
+        # 10 casillas en una columna seguida
+
+    def generate_tiles_coordinates(self):
+
+        last_x = 0
+        last_y = 0
+        for i in range (len(self.tiles_coordinates) - 1, -1, -1):
+            if self.tiles_coordinates[i][1] < self.current_y_loop:
+                del self.tiles_coordinates[i]
+
+        if len(self.tiles_coordinates) > 0:
+            last_coordinates = self.tiles_coordinates[-1]
+            last_y = last_coordinates[1] + 1
+            last_x = last_coordinates[0]
+        print("como fue")
+
+        for i in range(len(self.tiles_coordinates), self.NB_TILES):
+            r = random.randint(0, 2)
+            # 0 -> straight
+            # 1 -> right
+            # 2 -> left
+            start_index = -int(self.V_NB_LINES / 2) + 1
+            end_index = start_index + self.V_NB_LINES - 1
+            if last_x <= start_index:
+                r = 1
+            if last_x >= end_index:
+                r = 2
+            self.tiles_coordinates.append((last_x, last_y))
+            if r == 1:
+                last_x += 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+            if r == 2:
+                last_x -= 1
+                self.tiles_coordinates.append((last_x, last_y))
+                last_y += 1
+                self.tiles_coordinates.append((last_x, last_y))
+
+            last_y += 1
+
+        print("a ver")
 
     def init_vertical_lines(self):
         with self.canvas:
@@ -137,13 +208,15 @@ class MainWidget(Widget):
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
+        self.update_ship()
         self.current_offset_y += self.speed * time_factor
-        # self.current_offset_x += self.current_speed_x * time_factor
+        self.current_offset_x += self.current_speed_x * time_factor
         spacing_y = self.H_LINES_SPACING * self.height
         # spacing_x = self.V_LINES_SPACING * self.width
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
             self.current_y_loop += 1
+            self.generate_tiles_coordinates()
             print("loop: " + str(self.current_y_loop))
             # self.current_offset_x -= spacing_x SALTO DE LINEAS VERTICALES
         # print("dt: " + str(dt * 60))
